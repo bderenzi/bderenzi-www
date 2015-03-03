2bfd2e6816b422d12e966dddab2e3acdcc992ba7
Brian DeRenzi <bderenzi@gmail.com>
2015-03-03 15:21:57 +0300

    Added in education and research positions

--- app/src/components/bio/BioController.js
+++ app/src/components/bio/BioController.js
@@ -23,7 +23,56 @@
       'Community empowerment',
     ];
 
-    $scope.reserachInterests = reserachInterests;
+    var positions = [
+      {
+        start:       '2006',
+        end:         '2011',
+        title:       'Research Assistant',
+        institution: 'University of Washington',
+        department:  'Computer Science & Engineering',
+      },{
+        start:       '2012',
+        end:         '2013',
+        title:       'Research Associate',
+        institution: 'University of Washington',
+        department:  'Computer Science & Engineering',
+      },{
+        start:       '2013',
+        end:         '2015',
+        title:       'Research Scientist',
+        institution: 'IBM Research Africa',
+        department:  'Nairobi Lab',
+      },{
+        start:       '2015',
+        end:         'Present',
+        title:       'Senior Lecturer',
+        institution: 'University of Cape Town',
+        department:  'Computer Science',
+      },
+    ];
+
+    var education = [
+      {
+        degree:      'B.S.',
+        year:        '2006',
+        institution: 'University of California, Santa Barbara',
+        department:  'Computer Engineering',
+      },{
+        degree:      'M.S.',
+        year:        '2008',
+        institution: 'University of Washington',
+        department:  'Computer Science & Engineering',
+      },{
+        degree:      'Ph.D.',
+        year:        '2011',
+        institution: 'University of Washington',
+        department:  'Computer Science & Engineering',
+      },
+    ];
+
+    $scope.reserachInterests  = reserachInterests;
+    $scope.education          = education;
+    $scope.positions          = positions;
   }
 
 })();
