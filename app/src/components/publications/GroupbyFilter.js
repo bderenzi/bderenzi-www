71d2b89ad2916a16b6f02e79f17e178fd39ab9cd
Brian DeRenzi <bderenzi@gmail.com>
2015-03-03 14:11:45 +0300

    Make jshint happy

--- app/src/components/publications/GroupbyFilter.js
+++ app/src/components/publications/GroupbyFilter.js
@@ -19,52 +19,52 @@
    */
   angular.module('bdr')
     .filter('groupBy', ['$parse', function ($parse) {
-    return function (list, group_by) {
+    return function (list, groupBy) {
 
         var filtered = [];
-        var prev_item = null;
-        var group_changed = false;
+        var prevItem = null;
+        var groupChanged = false;
         // this is a new field which is added to each item where we append "_CHANGED"
         // to indicate a field change in the list
-        //was var new_field = group_by + '_CHANGED'; - JB 12/17/2013
-        var new_field = 'group_by_CHANGED';
+        //was var newField = group_by + '_CHANGED'; - JB 12/17/2013
+        var newField = 'group_by_CHANGED';
 
         // loop through each item in the list
         angular.forEach(list, function (item) {
 
-            group_changed = false;
+            groupChanged = false;
 
             // if not the first item
-            if (prev_item !== null) {
+            if (prevItem !== null) {
 
                 // check if any of the group by field changed
                 
                 //force group_by into Array
-                group_by = angular.isArray(group_by) ? group_by : [group_by]; 
+                groupBy = angular.isArray(groupBy) ? groupBy : [groupBy]; 
                 
                 //check each group by parameter
-                for (var i = 0, len = group_by.length; i < len; i++) {
-                    if ($parse(group_by[i])(prev_item) !== $parse(group_by[i])(item)) {
-                        group_changed = true;
+                for (var i = 0, len = groupBy.length; i < len; i++) {
+                    if ($parse(groupBy[i])(prevItem) !== $parse(groupBy[i])(item)) {
+                        groupChanged = true;
                     }
                 }
 
                 
             }// otherwise we have the first item in the list which is new
             else {
-                group_changed = true;
+                groupChanged = true;
             }
 
             // if the group changed, then add a new field to the item
             // to indicate this
-            if (group_changed) {
-                item[new_field] = true;
+            if (groupChanged) {
+                item[newField] = true;
             } else {
-                item[new_field] = false;
+                item[newField] = false;
             }
 
             filtered.push(item);
-            prev_item = item;
+            prevItem = item;
 
         });
 
--- app/src/components/publications/HighlightFilter.js
+++ app/src/components/publications/HighlightFilter.js
@@ -11,10 +11,11 @@
   angular.module('bdr')
     .filter('highlight', function($sce) {
       return function(text, phrase) {
-        if (phrase) text = text.replace(new RegExp('('+phrase+')', 'gi'),
-          '<span class="highlighted">$1</span>')
+        if (phrase) {
+          text = text.replace(new RegExp('('+phrase+')', 'gi'), '<span class="highlighted">$1</span>');
+        }
         
-        return $sce.trustAsHtml(text)
-      }
+        return $sce.trustAsHtml(text);
+      };
     });
 })();
