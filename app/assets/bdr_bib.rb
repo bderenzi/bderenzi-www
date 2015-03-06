b0b622bbb690f4ba75be8c0a43552af76afaacd0
Brian DeRenzi <bderenzi@gmail.com>
2015-03-06 14:28:50 +0300

    Ruby file to convert bibtex to json
    This does not go here, but keeping it for the time being

---
+++ app/assets/bdr_bib.rb
@@ -0,0 +1,9 @@
+require 'bibtex'
+require 'json'
+
+# convert bibtex to json
+b = BibTeX.open('./bderenzi_citations_clean.bib')
+File.open('bderenzi_citations.json', 'w') do |f|
+	out = JSON.pretty_generate(JSON.parse(b.to_json)).to_s
+	f.write(out.gsub!('"{','"').gsub!('}"','"'))
+end
