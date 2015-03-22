a991c480c1fe890f3d7f10c17a9daa2821a15dde
Brian DeRenzi <bderenzi@gmail.com>
2015-03-22 19:01:52 +0200

    fix(calendar) - make linter happy

--- app/src/components/calendar/CalendarController.js
+++ app/src/components/calendar/CalendarController.js
@@ -44,9 +44,9 @@
 
     // Exported functions
     function datesMatch(d1, d2) {
-      return  (d1.getDate() == d2.getDate()) && 
-              (d1.getMonth() == d2.getMonth()) &&
-              (d1.getFullYear() == d2.getFullYear());
+      return  (d1.getDate() === d2.getDate()) && 
+              (d1.getMonth() === d2.getMonth()) &&
+              (d1.getFullYear() === d2.getFullYear());
     }
 
     function getTZString() {
